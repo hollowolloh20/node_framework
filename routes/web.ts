9785c24b8b0f0e9f50117e4144f23f7ee2455284
Ever Hollow <hollowolloh2020@protonmail.com>
2021-02-01 05:28:01 -0900

    main/feature/clear

--- routes/web.ts
+++ routes/web.ts
@@ -1,50 +1,5 @@
 import Router from '../framework/router/router';
-import Middle from '../app/http/middleware/someCheck';
-import Middle2 from '../app/http/middleware/someCheck2';
-import Middle3 from '../app/http/middleware/someCheck3';
-import Middle4 from '../app/http/middleware/someCheck4';
 
 const router = new Router();
-
-router
-  .get('/index', (ctx) => {
-    ctx.response.send(
-      JSON.stringify({
-        result: 'success',
-      })
-    );
-  })
-  .useBefore([new Middle()]);
-
-router
-  .get('/index2', (ctx) => {
-    ctx.response.send(
-      JSON.stringify({
-        result: 'success',
-      })
-    );
-  })
-  .useBefore([new Middle2()]);
-
-router
-  .get('/index3', (ctx) => {
-    ctx.response.send(
-      JSON.stringify({
-        result: 'success',
-      })
-    );
-  })
-  .useBefore([new Middle3()]);
-
-router
-  .get('/index4', (ctx) => {
-    ctx.response.send(
-      JSON.stringify({
-        result: 'success',
-      })
-    );
-  })
-  .useBefore([new Middle3(), new Middle3()])
-  .useAfter([new Middle4(), new Middle4()]);
 
 export default router;
